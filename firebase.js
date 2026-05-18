dcee51b1d15c05bbd277837f584e5606ace05f5c
boopathyk286-debug <boopathyk286@gmail.com>
2026-05-18 11:52:41 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,29 +1,16 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore";
-import { getDatabase } from "firebase/database";
-import { getAuth } from "firebase/auth";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
-// Your web app's Firebase configuration
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyAuE95hSCbvKPG2eq3ICTanW3dPxYvsXag",
   authDomain: "nama-game-2bb2d.firebaseapp.com",
   projectId: "nama-game-2bb2d",
   storageBucket: "nama-game-2bb2d.firebasestorage.app",
   messagingSenderId: "1051522618429",
-  appId: "1:1051522618429:web:2f84101ca7abdddc948654",
-  measurementId: "G-8LZERLFEDD"
+  appId: "1:1051522618429:web:2f84101ca7abdddc948654"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Services
-const analytics = getAnalytics(app);
-const db = getFirestore(app);
-const realtimeDB = getDatabase(app);
-const auth = getAuth(app);
-
-// Export
-export { app, analytics, db, realtimeDB, auth };
+export const db = getFirestore(app);
